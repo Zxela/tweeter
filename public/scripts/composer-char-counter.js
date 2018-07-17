b010c077e86ad6e12c9a7da03b7a5e900e65e4e8
Your Name <alex.zvaniga@gmail.com>
2018-07-17 18:47:14 -0400

    adding new tweet html

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,5 +1,13 @@
 $(document).ready(function() {
-    $(".new-tweet textarea").on('keydown', function() {
-        console.log($(this).val().length);
+    $(".new-tweet textarea").on('keyup', function() { //Counter
+        let $len = $(this).val().length; // assign varibale to number of characters typed
+        let count = 140 - $len;
+        let $counter = $(this).siblings('.counter')
+        $counter.text(count);
+        if (count <= 0) {
+            $counter.addClass('overcount');
+        } else {
+            $counter.removeClass('overcount');
+        }
     })
 });
