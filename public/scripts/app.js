52faa63cff8a566ca736dcf74a2b79b9a74335f2
Your Name <alex.zvaniga@gmail.com>
2018-07-18 16:42:13 -0400

    added highlight around buttons

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,7 +1,17 @@
 $(document).ready(function() {
-  //render tweets
+  const $tweetSub = $("#postTweets"); //jquery submit
+  const loadTweets = function () {
+    $.getJSON("/tweets", function(data){
+      renderTweets(data);
+    })
+    .fail(function(){
+      alert("failed to load tweets from JSON");
+    })
+  }
+//render tweets start
   function renderTweets(arrOfTweets) {
-    for (let i = 0; i < arrOfTweets.length; i++) {
+      $('.tweetContainer').empty();
+      for (let i = 0; i < arrOfTweets.length; i++) {
       createTweetElement(arrOfTweets[i]);
     }
   }
@@ -9,7 +19,8 @@ $(document).ready(function() {
     let d = new Date();
     let tDay = d.getTime();
     let postTime = Math.floor((tDay - tweets.created_at) / 86400000); //num of ms in a day
-    $(".tweetContainer").append(`<article>
+    //prepending to get newest post first
+    $(".tweetContainer").prepend(`<article> 
         <header>
           <img src="${tweets.user.avatars["small"]}" class="userPic">
           <div class="userName">${tweets.user.name}</div>
@@ -21,30 +32,24 @@ $(document).ready(function() {
         </footer>
       </article>`);
   }
-  let $tweetSub = $("#postTweets");
   $tweetSub.on("submit", function(ev) {
     ev.preventDefault();
     let str = $('#postTweets').find('textarea').val()
-    console.log("new tweet", '"' + str + '"', "submitted"); //logs tweet submitted with text
     let $urlData = $(this).serialize();
     if (str === null || str === ""){
-      alert("Null or empty string")
+      alert("Please write something first!")
     }else if (str.length > 0 && str.length < 141) { //if text is greater than 0 or less than 141
+      console.log("new tweet", '"' + str + '"', "submitted"); //logs tweet submitted with text
       $.post("/tweets", $urlData);
-      return;
+      loadTweets();
     } else {
-      alert("Too few or too many characters!")
+      alert("Please keep character count below 140!");
     }
     
   });
-  let loadTweets = function () {
-    $.getJSON("/tweets", function(data){
-      renderTweets(data)
-    })
-    .fail(function(){
-      alert("failed to load tweets from JSON")
-    })
-  }
   loadTweets();
-  
+
+
+
+// end of onDocumentReady
 });
