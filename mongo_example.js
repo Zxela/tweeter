4f0d53e3b8583f73f8c80a7ba06c64e24d93e48a
Your Name <alex.zvaniga@gmail.com>
2018-07-19 15:36:09 -0400

    moved get route to query mongo db

--- mongo_example.js
+++ mongo_example.js
@@ -1,5 +1,12 @@
 "use strict";
 
+// Basic express setup:
+
+const PORT          = 8080;
+const express       = require("express");
+const bodyParser    = require("body-parser");
+const app           = express();
+// mongo setup
 const MongoClient = require("mongodb").MongoClient;
 const MONGODB_URI = "mongodb://localhost:27017/tweeter";
 
@@ -9,25 +16,49 @@ MongoClient.connect(MONGODB_URI, (err, db) => {
     throw err;
   }
 
-  // ==> We have a connection to the "test-tweets" db,
-  //     starting here.
+  // We have a connection to the "tweeter" db, starting here.
   console.log(`Connected to mongodb: ${MONGODB_URI}`);
+  app.use(bodyParser.urlencoded({ extended: true }));
+  app.use(express.static("public"));
 
-  // ==> In typical node-callback style, any program
-  //     logic that needs to use the connection needs
-  //     to be invoked from within here.
-  //
-  // Another way to say: this is an "entry point" for
-  // a database-connected application!
-  db.collection("tweets").find({}, (err, result) => {
-  // error handling
-    if (err) throw err;
-  // log results
-    console.log("find result: ", result);
-    console.log("type of find result: ", typeof result);
-  
 
-  // ==> At the end, we close the connection:
-  db.close();
+  const db = **;
+  const DataHelpers = require("./lib/data-helpers.js")(db);
+
+// The `tweets-routes` module works similarly: we pass it the `DataHelpers` object
+// so it can define routes that use it to interact with the data layer.
+  const tweetsRoutes = require("./routes/tweets")(DataHelpers);
+
+// Mount the tweets routes at the "/tweets" path prefix:
+  app.use("/tweets", tweetsRoutes);
+
+  app.listen(PORT, () => {
+  console.log("Example app listening on port " + PORT);
   });
+  // ==> Refactored and wrapped as new, tweet-specific function:
+
+  function getTweets(callback) {
+    db.collection("tweets").find().toArray((err, tweets) => {
+      if (err) {
+        return callback(err);
+      }
+      callback(null, tweets);
+    });
+  }
+
+  // ==> Later it can be invoked. Remember even if you pass
+  //     `getTweets` to another scope, it still has closure over
+  //     `db`, so it will still work. Yay!
+
+  getTweets((err, tweets) => {
+    if (err) throw err;
+
+    console.log("Logging each tweet:");
+    for (let tweet of tweets) {
+      console.log(tweet);
+    }
+
+    db.close();
+  });
+
 });
