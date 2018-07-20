e1a2683ade6f952ee0ecd98360695e7179caffba
Your Name <alex.zvaniga@gmail.com>
2018-07-20 12:59:06 -0400

    updated package.json

--- server/index.js
+++ server/index.js
@@ -2,37 +2,37 @@
 
 // Basic express setup:
 
-const PORT          = 8080;
-const express       = require("express");
-const bodyParser    = require("body-parser");
-const app           = express();
-// mongo setup
+const PORT = 8080;
+const express = require("express");
+const bodyParser = require("body-parser");
+const app = express();
+// Mongo Setup
 const MongoClient = require("mongodb").MongoClient;
 const MONGODB_URI = "mongodb://localhost:27017/tweeter";
 
-MongoClient.connect(MONGODB_URI, (err, db) => {
-  if (err) {
-    console.error(`Failed to connect: ${MONGODB_URI}`);
-    throw err;
-  }
+MongoClient.connect(
+	MONGODB_URI,
+	(err, db) => {
+		if (err) {
+			console.error(`Failed to connect: ${MONGODB_URI}`);
+			throw err;
+		}
 
-  // We have a connection to the "tweeter" db, starting here.
-  console.log(`Connected to mongodb: ${MONGODB_URI}`);
-  app.use(bodyParser.urlencoded({ extended: true }));
-  app.use(express.static("public"));
-  const DataHelpers = require("./lib/data-helpers.js")(db);
+		// We have a connection to the "tweeter" db, starting here.
+		console.log(`Connected to mongodb: ${MONGODB_URI}`);
+		app.use(bodyParser.urlencoded({ extended: true }));
+		app.use(express.static("public"));
+		const DataHelpers = require("./lib/data-helpers.js")(db);
 
-// The `tweets-routes` module works similarly: we pass it the `DataHelpers` object
-// so it can define routes that use it to interact with the data layer.
-  const tweetsRoutes = require("./routes/tweets")(DataHelpers);
+		// The `tweets-routes` module works similarly: we pass it the `DataHelpers` object
+		// so it can define routes that use it to interact with the data layer.
+		const tweetsRoutes = require("./routes/tweets")(DataHelpers);
 
-// Mount the tweets routes at the "/tweets" path prefix:
-  app.use("/tweets", tweetsRoutes);
+		// Mount the tweets routes at the "/tweets" path prefix:
+		app.use("/tweets", tweetsRoutes);
 
-  app.listen(PORT, () => {
-  console.log("Example app listening on port " + PORT);
-  });
-  // ==> Refactored and wrapped as new, tweet-specific function:
-
-  
-});
+		app.listen(PORT, () => {
+			console.log("Example app listening on port " + PORT);
+		});
+	}
+);
