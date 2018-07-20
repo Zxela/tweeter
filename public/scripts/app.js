6a14d5cc97a3b7df68f645e8e4915d8d551cedeb
Your Name <alex.zvaniga@gmail.com>
2018-07-20 12:40:36 -0400

    cleaned spacing/semicolons

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,34 +1,35 @@
 $(document).ready(function() {
-  
-  const loadTweets = function () {
-    $.getJSON("/tweets", function(data){
-      renderTweets(data);
-    })
-    .fail(function(){
-      $("#errorMsg").text("failed to load tweets").fadeIn();
-      $(".new-tweet textarea").on('focus', function(){
-        $("#errorMsg").fadeOut();
-      });
-    })
-  }
-//render tweets start
-  function renderTweets(arrOfTweets) {
-    $('.tweetContainer').empty(); //empty tweets on site
-    for (let i = 0; i < arrOfTweets.length; i++) {
-    createTweetElement(arrOfTweets[i]);
-    }
-  }
-  function createTweetElement(tweets) {
-    let d = new Date();
-    let tDay = d.getTime();
-    let postTime = Math.floor((tDay - tweets.created_at) / 86400000); //num of ms in a day
-    //prepending to get newest post first
-    function escape(str) {
-      var div = document.createElement('div');
-      div.appendChild(document.createTextNode(str));
-      return div.innerHTML;
-    }
-    $(".tweetContainer").prepend(`<article> 
+	const loadTweets = function() {
+		$.getJSON("/tweets", function(data) {
+			renderTweets(data);
+		}).fail(function() {
+			$("#errorMsg")
+				.text("failed to load tweets")
+				.fadeIn();
+			$(".new-tweet textarea").on("focus", function() {
+				$("#errorMsg").fadeOut();
+			});
+		});
+	};
+	//render tweets start
+	function renderTweets(arrOfTweets) {
+		$(".tweetContainer").empty(); //empty tweets on site
+		for (let i = 0; i < arrOfTweets.length; i++) {
+			createTweetElement(arrOfTweets[i]);
+		}
+	}
+	function createTweetElement(tweets) {
+		let d = new Date();
+		let tDay = d.getTime();
+		let postTime = Math.floor((tDay - tweets.created_at) / 86400000); //num of ms in a day
+		//prepending to get newest post first
+		function escape(str) {
+			//escape function to prevent user from injecting js, html.
+			var div = document.createElement("div");
+			div.appendChild(document.createTextNode(str));
+			return div.innerHTML;
+		}
+		$(".tweetContainer").prepend(`<article> 
         <header>
           <img src="${tweets.user.avatars["small"]}" class="userPic">
           <div class="userName">${tweets.user.name}</div>
@@ -45,37 +46,46 @@ $(document).ready(function() {
           <i class="fas fa-heart"></i>
         </footer>
       </article>`);
-  }
-  //jquery submit button "posttweet"
-  $("#postTweets").on("submit", function(ev) {
-    ev.preventDefault();
-    let str = $('#postTweets').find('textarea').val()
-    let $urlData = $(this).serialize();
-    if (str === null || str === ""){
-      $("#errorMsg").text("Please write something first!").fadeIn();
-      $(".new-tweet textarea").on('focus', function(){
-        $("#errorMsg").fadeOut();
-      });
-    }else if (str.length > 0 && str.length < 141) { //if text is greater than 0 or less than 141
-      $.post("/tweets", $urlData, function(){
-        $(".new-tweet textarea").val('');
-        $(".new-tweet .counter").text('140');
-        loadTweets(); //load tweets from updated json
-      }); 
-    } else {
-      $("#errorMsg").text("Too many characters!").fadeIn();
-      $(".new-tweet textarea").on('focus', function(){
-        $("#errorMsg").fadeOut();
-      })
-    }
-  });
-// event listener for button
-  const $compose = $("#composeTweet");
-  $compose.on('click', function() {
-    $(".new-tweet").slideToggle();
-    $(".new-tweet textarea").focus();
-  })
- //First load of tweets 
-  loadTweets();
-// end of onDocumentReady
+	}
+	//jquery submit button "posttweet"
+	$("#postTweets").on("submit", function(ev) {
+		//on pressing submit button
+		ev.preventDefault(); //prevent default event from occuring
+		let str = $("#postTweets")
+			.find("textarea")
+			.val();
+		let noSpaceStr = str.trim(); //catch line breaks
+		let $urlData = $(this).serialize();
+		if (noSpaceStr === null || noSpaceStr === "") {
+			$("#errorMsg")
+				.text("Please write something first!")
+				.fadeIn();
+			$(".new-tweet textarea").on("focus", function() {
+				$("#errorMsg").fadeOut();
+			});
+		} else if (str.length > 0 && str.length < 141) {
+			//If text meets reqs
+			$.post("/tweets", $urlData, function() {
+				$(".new-tweet textarea").val(""); //reset text area on submit
+				$(".new-tweet .counter").text("140"); //reset counter on submit
+				loadTweets(); //load tweets from updated json
+			});
+		} else {
+			$("#errorMsg")
+				.text("Too many characters!")
+				.fadeIn();
+			$(".new-tweet textarea").on("focus", function() {
+				$("#errorMsg").fadeOut();
+			});
+		}
+	});
+	// event listener for button
+	const $compose = $("#composeTweet");
+	$compose.on("click", function() {
+		$(".new-tweet").slideToggle();
+		$(".new-tweet textarea").focus();
+	});
+	//First load of tweets
+	loadTweets();
+	// end of onDocumentReady
 });
--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,13 +1,14 @@
 $(document).ready(function() {
-    $(".new-tweet textarea").on("input", function() { //Counter
-        let $len = $(this).val().length; // assign varibale to number of characters typed
-        let count = 140 - $len;
-        let $counter = $(this).siblings('.counter')
-        $counter.text(count);
-        if (count <= 0) {
-            $counter.addClass('overcount');
-        } else {
-            $counter.removeClass('overcount');
-        }
-    })
+	$(".new-tweet textarea").on("input", function() {
+		//Counter
+		let $len = $(this).val().length; // assign varibale to number of characters typed
+		let count = 140 - $len;
+		let $counter = $(this).siblings(".counter");
+		$counter.text(count);
+		if (count <= 0) {
+			$counter.addClass("overcount");
+		} else {
+			$counter.removeClass("overcount");
+		}
+	});
 });
