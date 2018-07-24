50f0782c4f4a662187666213a18bd33e63a2df13
Your Name <alex.zvaniga@gmail.com>
2018-07-24 11:36:14 -0400

    reminder to add increment

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
-	/*run on document ready */
+	//run nested functions only once document has loaded
 	const loadTweets = function() {
 		$(".tweetContainer").empty(); //empty tweets on site
 		$.getJSON("/tweets", function(data) {
@@ -22,38 +22,23 @@ $(document).ready(function() {
 		const $like = $(".likes");
 		$like.on("click", function() {
 			console.log("clicked", $(this).children("i"));
-			$heart = $(this).children("i");
+			let $heart = $(this).children("i");
+			let $article = $(this)
+				.closest("article")
+				.get();
 			if ($heart.hasClass("liked")) {
 				$heart.removeClass("liked");
-				console.log(
-					$(this)
-						.closest("article")
-						.get()
-				);
-				$.post(
-					`/tweets/${$(this)
-						.closest("article")
-						.get()}/removeLike`,
-					function() {
-						console.log("remove CB successful"); //placeholder for remove class
-					}
-				);
+
+				$.post(`/tweets/${$article}/removeLike`, function() {
+					console.log("remove CB successful"); //placeholder for remove class -use INCREMENT
+				});
 			} else {
 				$heart.addClass("liked");
-				console.log(
-					$(this)
-						.closest("article")
-						.get()
-				);
+				console.log($article);
 
-				$.post(
-					`/tweets/${$(this)
-						.closest("article")
-						.get()}/addLike`,
-					function() {
-						console.log("add CB successful"); //placeholder for addclass
-					}
-				);
+				$.post(`/tweets/${$article}/addLike`, function() {
+					console.log("add CB successful"); //placeholder for addclass -use INCREMENT
+				});
 			}
 		});
 		//END OF LIKE LISTENER
@@ -89,20 +74,17 @@ $(document).ready(function() {
 					</span>
         </footer>
       </article>`);
-
-		//event listener for likes - nested to ensure attached to each tweet
 	}
-	/* <i class="fas fa-heart"></i>  icon for heart when liked...*/
-
-	//jquery submit button "posttweet"
+	//Event Listener - submit "tweet" button
 	$("#postTweets").on("submit", function(ev) {
 		ev.preventDefault(); //prevent default event from occuring
 		let str = $("#postTweets")
 			.find("textarea")
 			.val();
 		let noSpaceStr = str.trim(); //catch line breaks
-		let $urlData = $(this).serialize();
+		let $urlData = $(this).serialize(); //serialize data
 		if (noSpaceStr === null || noSpaceStr === "") {
+			//is message empty?
 			$("#errorMsg")
 				.text("Please write something first!")
 				.fadeIn();
@@ -110,6 +92,7 @@ $(document).ready(function() {
 				$("#errorMsg").fadeOut();
 			});
 		} else if (str.length > 0 && str.length < 141) {
+			//is message between 0 and 141 characters?
 			//If text meets reqs
 			$.post("/tweets", $urlData, function() {
 				$(".new-tweet textarea").val(""); //reset text area on submit
@@ -117,6 +100,7 @@ $(document).ready(function() {
 				loadTweets(); //load tweets from updated json
 			});
 		} else {
+			//it must be over!
 			$("#errorMsg")
 				.text("Too many characters!")
 				.fadeIn();
@@ -125,7 +109,7 @@ $(document).ready(function() {
 			});
 		}
 	});
-	// event listener for "Compose" button
+	// Event Listener - "Compose" button
 	const $compose = $("#composeTweet");
 	$compose.on("click", function() {
 		$(".new-tweet").slideToggle();
@@ -134,6 +118,4 @@ $(document).ready(function() {
 
 	//First load of tweets
 	loadTweets();
-
-	// end of onDocumentReady
-});
+}); // end of onDocumentReady
